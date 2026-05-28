72119a76e8877b1de5376c4b9ab74840f364c7e3
Theodor11-fcb <theosticha@icloud.com>
2026-05-28 12:41:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const activityForm = document.querySelector('#activity-form');
-activityForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log('Klik funguje');
-});
-export {};
